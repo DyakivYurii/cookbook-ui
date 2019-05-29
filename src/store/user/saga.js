9e5cb541d629bde485389d29a27e48d307c0bf92
Yurii <dyakivyurii@gmail.com>
2019-05-29 23:00:19 +0300

    [feature] Created store functionality with comunication with a server

--- src/store/user/saga.js
+++ src/store/user/saga.js
@@ -1,10 +1,18 @@
 import { takeEvery, call, put } from 'redux-saga/effects';
 import USER from './types';
 
-import { getUserInfoAPI } from './services';
+import {
+  getUserInfoAPI,
+  getMyUserInfoAPI,
+  putUserAPI,
+  deleteUserAPI
+} from './services';
 
 function* watcherRecipes() {
   yield takeEvery(USER.GET_INFO_REQUEST, getUserInfoAsync);
+  yield takeEvery(USER.MY_GET_INFO_REQUEST, getMyUserInfoAsync);
+  yield takeEvery(USER.PUT_INFO_REQUEST, putUserAsync);
+  yield takeEvery(USER.DELETE_USER_REQUEST, deleteUserAsync);
 }
 
 function* getUserInfoAsync(action) {
@@ -16,6 +24,36 @@ function* getUserInfoAsync(action) {
     });
   } catch (error) {
     yield put({ type: USER.GET_INFO_FAILURE });
+  }
+}
+
+function* getMyUserInfoAsync(action) {
+  try {
+    const user = yield call(getMyUserInfoAPI, action.payload);
+    yield put({
+      type: USER.GET_INFO_SUCCESS,
+      payload: { name: user.name, email: user.email }
+    });
+  } catch (error) {
+    yield put({ type: USER.GET_INFO_FAILURE });
+  }
+}
+
+function* putUserAsync(action) {
+  try {
+    yield call(putUserAPI, action.payload);
+    yield put({ type: USER.PUT_INFO_SUCCESS });
+  } catch (error) {
+    yield put({ type: USER.PUT_INFO_FAILURE });
+  }
+}
+
+function* deleteUserAsync(action) {
+  try {
+    yield call(deleteUserAPI, action.payload);
+    yield put({ type: USER.DELETE_USER_SUCCESS });
+  } catch (error) {
+    yield put({ type: USER.DELETE_USER_FAILURE });
   }
 }
 
--- src/store/user/services.js
+++ src/store/user/services.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-export const getUserInfoAPI = ({ token }) => {
+export const getUserInfoAPI = ({ id }) => {
+  return axios.get(`${process.env.REACT_APP_API_URL}/api/users/user/${id}`);
+};
+
+export const getMyUserInfoAPI = ({ token }) => {
   return axios
     .get(`${process.env.REACT_APP_API_URL}/api/users/me`, {
       headers: { Authorization: `Bearer ${token}` }
@@ -8,4 +12,24 @@ export const getUserInfoAPI = ({ token }) => {
     .then((result) => {
       return result.data.data[0];
     });
+};
+
+export const putUserAPI = ({ token, userInfo }) => {
+  return axios
+    .get(
+      `${process.env.REACT_APP_API_URL}/api/users/me`,
+      { ...userInfo },
+      {
+        headers: { Authorization: `Bearer ${token}` }
+      }
+    )
+    .then((result) => {
+      return result.data.data[0];
+    });
+};
+
+export const deleteUserAPI = ({ token }) => {
+  return axios.get(`${process.env.REACT_APP_API_URL}/api/users/me`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
 };
